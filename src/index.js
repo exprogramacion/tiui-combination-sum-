31757741a614a8c3dbcccb805255afb46cb6cf4d
Kairos Daniel Elihu Diaz Delgado <nem.daniele@coppel.com>
2024-07-02 14:05:59 -0600

    CombinationSum - Recursividad | Prueba 3

--- src/index.js
+++ src/index.js
@@ -1,15 +1,33 @@
 /**
  * @param {number[]} candidates - candidate numbers we're picking from.
- * @param {number} remainingSum - remaining sum after adding candidates to currentCombination.
  * @param {number[][]} finalCombinations - resulting list of combinations.
  * @param {number[]} currentCombination - currently explored candidates.
  * @param {number} startFrom - index of the candidate to start further exploration from.
  * @return {number[][]}
  */
 
-const combinationSumRecursive = (
-    
-  }
+const combinationSumRecursive = (candidates, target) => {
+  const outData = [];
+  
+  const doSumRecursive = (startFrom, candidates, target, finalCombinations, currentCombination) => {
+      if (startFrom === candidates.length) {
+          if (target === 0) {
+              finalCombinations.push([...currentCombination]);
+          }
+          return;
+      }
+      if (candidates[startFrom] <= target) {
+          currentCombination.push(candidates[startFrom]);
+          doSumRecursive(startFrom, candidates, target - candidates[startFrom], finalCombinations, currentCombination);
+          currentCombination.pop();
+      }
+      doSumRecursive(startFrom + 1, candidates, target, finalCombinations, currentCombination);
+  };
+  
+  doSumRecursive(0, candidates, target, outData, []);
+  return outData;
+};
+
   
   /**
    * Backtracking algorithm of finding all possible combination for specific sum.
